cb5f4d059745e7cb63ccfeb68936b137078d5c05
Eryckson Magno <eryckson@albedo.team>
2021-03-05 17:17:01 +0000

    Merged PR 124: docker + k8s

---
+++ src/local.tf
@@ -0,0 +1,55 @@
+﻿terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_namespace" "identity" {
+  metadata {
+    name = "identity-business"
+  }
+}
+
+resource "kubernetes_deployment" "identity" {
+  metadata {
+    name = "identity-business"
+    namespace = kubernetes_namespace.identity.metadata.0.name
+    labels = {
+      app = "IdentityBusiness"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "identity-business"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "identity-business"
+        }
+      }
+      spec {
+        container {
+          image = "identity-business:latest"
+          name = "identity-business-container"
+          image_pull_policy = "IfNotPresent"
+          port {
+            container_port = 80
+            protocol = "TCP"
+          }
+        }
+      }
+    }
+  }
+}
